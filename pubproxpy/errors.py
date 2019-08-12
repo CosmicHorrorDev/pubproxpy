fbc1b6ffa373625d01ef1fdd23c902b3cc8893c0
LovecraftianHorror <LovecraftianHorror@pm.me>
2019-08-11 22:36:22 -0400

    Dump (Too much unorganized work)

--- pubproxpy/errors.py
+++ pubproxpy/errors.py
@@ -1,3 +1,23 @@
+# TODO: call the super on exception to set the error message in here
+INVALID_API_RESP = (
+    "Invalid API. Get your API to make unlimited requests at"
+    "  http://pubproxy.com/#premium"
+)
+
+RATE_LIMIT_RESP = (
+    "We have to temporarily stop you. You're requesting proxies a little"
+    " too fast (2+ requests per second). Get your API to remove this limit"
+    " at http://pubproxy.com/#premium"
+)
+
+DAILY_LIMIT_RESP = (
+    "You have reached the maximum 50 requests for today. Get your API to"
+    " make unlimited requests at http://pubproxy.com/#premium"
+)
+
+NO_PROXY_RESP = "No proxy"
+
+
 class ProxyError(Exception):
     """Generic base error
     """
@@ -15,4 +35,9 @@ class RateLimitError(ProxyError):
 
 class DailyLimitError(ProxyError):
     """Error for hitting the daily request limit
+    """
+
+
+class NoProxyError(ProxyError):
+    """Error for getting the "No Proxy" response
     """
