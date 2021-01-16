46546310a94bd3075b8fc32a469759175894804a
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-01-16 18:41:44 -0500

    Version Bump

--- pubproxpy/__init__.py
+++ pubproxpy/__init__.py
@@ -2,4 +2,4 @@ from pubproxpy.fetcher import ProxyFetcher
 from pubproxpy.types import Level, Protocol
 
 __all__ = ["ProxyFetcher", "Level", "Protocol"]
-__version__ = "2.0.1"
+__version__ = "2.0.2"
